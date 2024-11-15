e324d2d39de4ea62917aea0f691880e0d324d256
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2024-11-15 03:05:19 +0000

    Funções base para CSV

---
+++ src/csv_operations.h
@@ -0,0 +1,18 @@
+#ifndef CSV_OPERATIONS_H
+#define CSV_OPERATIONS_H
+
+#include <stdbool.h>
+
+bool verificar_registro(const char *filename, const char *sala, const char *data, const char *horario_inicio, const char *horario_fim);
+
+int agendar_horario_sala(const char *filename, const char *sala, const char *data, const char *horario_inicio, const char *horario_fim);
+
+void apagar_reserva(const char *filename, const char *sala, const char *data, const char *horario_inicio, const char *horario_fim);
+
+void exibir_reservas(const char *filename);
+
+int cadastrar_usuario(const char *filename, const char login[20], const char senha[40]);
+
+int autenticar_usuario(const char *filename, const char login[20], const char senha[40]);
+
+#endif // CSV_OPERATIONS_H
