0c06755788f8a33fa14f783e11a3b30d81b97b66
Borges <devgabrielsborges@proton.me>
2024-11-20 22:26:06 -0300

    Base Glade files to project

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
 #include "csv_operations.h"
 
 /*
@@ -17,7 +19,27 @@
     int autenticar_usuario(const char *filename, const char login[20], const char senha[40]);
 */
 
-int main()
-{
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    GtkBuilder *builder;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new_from_file("interface.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+
+    if (!window) {
+        g_print("Erro ao carregar a janela principal!\n");
+        return -1;
+    }
+
+    gtk_builder_connect_signals(builder, NULL);
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
 }
+
+
+// gcc -o my_program main.c `pkg-config --cflags --libs gtk+-3.0`
+
